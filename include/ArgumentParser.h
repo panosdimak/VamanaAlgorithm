0fef9717658cb5955a3f2139c13a3a45b47959cd
sdi2000156 <sdi2000156@di.uoa.gr>
2024-12-02 14:26:22 +0200

    ImplementArgumentParser

---
+++ include/ArgumentParser.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <getopt.h>
+
+using namespace std;
+
+struct ParsedArguments
+{
+    int K = 0;
+    int L = 0;
+    int R = 0;
+    double Alpha = 0.0;
+    string BaseDatasetPath;
+    string QueryDatasetPath;
+    string GroundTruthPath;
+};
+
+class ArgumentParser
+{
+public:
+    static ParsedArguments ParseArguments(int argc, char *argv[]);
+    static void DisplayParsedArguments(const ParsedArguments &args);
+
+private:
+    static void Validate(const ParsedArguments &args);
+};
